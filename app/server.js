29bea54820116754d41b4f961a1ef291943ceaa5
Rajan Kumar Gautam <annapurna.arden@gmail.com>
2026-09-22 05:48:44 +0000

    initial commit-Devops-Gitops-Project

---
+++ app/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello! My Devops Gitops Project is running."); 
+});
+
+
+app.get("/health" , (req, res) => {
+    res.json({
+      status: "healthy"
+});
+});
+
+app.listen(3000,() =>{
+   console.log("Application running on port 3000");
+});
